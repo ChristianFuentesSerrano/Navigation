41a753676062f07c16fcf24a7b9c4e2a7806cfb6
Christian <cfs.christianfuentes@gmail.com>
2022-03-30 12:27:19 -0600

    Recylcer view

--- app/src/main/java/com/example/navigation/MainActivity.kt
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.navigation.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
@@ -18,9 +19,21 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        /*
         val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainNavHost) as NavHostFragment
         val navController = navHostFragment.navController
         navView.setupWithNavController(navController)
+        */
+
+        val fakeVideoData: Array<JSONObject> = arrayOf(
+            JSONObject("{\"title\": \"An awesome video title\", \"channel\": \"ChannelName\", \"views\": \"6k views\", \"datePosted\": \"5 days ago\", \"duration\": \"05:00\"}"),
+            JSONObject("{\"title\": \"John\", \"channel\": \"Channel\", \"views\": \"8k views\", \"datePosted\": \"5 days ago\", \"duration\": \"05:00\"}"),
+            JSONObject("{\"title\": \"John\", \"channel\": \"Channel\", \"views\": \"10k views\", \"datePosted\": \"5 days ago\", \"duration\": \"05:25\"}"),
+            JSONObject("{\"title\": \"John\", \"channel\": \"Channel\", \"views\": \"150k views\", \"datePosted\": \"A month ago\", \"duration\": \"05:02\"}"),
+            JSONObject("{\"title\": \"John\", \"channel\": \"Channel\", \"views\": \"254k views\", \"datePosted\": \"2 years ago\", \"duration\": \"05:00\"}")
+        )
+
+        binding.rvVideoEntries.adapter = MainAdapter(fakeVideoData)
     }
 }
